aab5a5d257aca912c0e2761df7bb43e38d4b5178
davisidarta <44589562+davisidarta@users.noreply.github.com>
2019-12-16 00:16:34 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -21,12 +21,8 @@ def Run_Diffusion(data, n_components=50, knn=30, n_jobs=-1, alpha=1, force_spars
 	# Determine the kernel
 	N = data.shape[0]
 	if not issparse(data):
-		if force_sparse:
-			print('Converting from dense to sparse matrix. Determing nearest neighbor graph...')
-			data=data.tocsr()
-		else:
-			print('Dense matrix input. Using sparse matrices is recommended for scalability. Determing nearest neighbor graph...')
-		
+		print('Converting from dense to sparse matrix. Determing nearest neighbor graph...')
+		data=data.tocsr()
 		nbrs = NearestNeighbors(n_neighbors=int(knn), metric = 'euclidean', n_jobs=n_jobs).fit(data.values)
 		kNN = nbrs.kneighbors_graph(data.values, mode='distance')
 

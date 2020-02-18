f0c2c672130188869d3a07cb719fb7a83be0a8b7
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-02-18 11:42:16 -0300

    Update common.py

--- src/dbmap/common.py
+++ src/dbmap/common.py
@@ -4,28 +4,6 @@ Utility functions
 
 import numpy as np
 import pandas as pd
-import networkx as nx
-import time
-import random
-import copy
-
-from sklearn.metrics import pairwise_distances
-from sklearn import preprocessing
-from sklearn.neighbors import NearestNeighbors
-from joblib import Parallel, delayed
-from scipy.sparse.linalg import eigs
-
-from scipy.sparse import csr_matrix, find, csgraph
-from scipy.stats import entropy, pearsonr, norm
-from numpy.linalg import inv
-from copy import deepcopy
-from palantir.presults import PResults
-
-import warnings
-warnings.filterwarnings(action="ignore", message="scipy.cluster")
-warnings.filterwarnings(action="ignore", module="scipy",
-                        message="Changing the sparsity")
-
 import matplotlib.pyplot as plt
 
 def embedding_plot(X, title, show_images = True):

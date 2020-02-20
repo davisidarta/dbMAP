5595bad895b6732417465d47861a58bb94076c8a
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-02-20 11:48:15 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -6,44 +6,43 @@ from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix, find, issparse
 from scipy.sparse.linalg import eigs
 import tables
-import scanpy as sc
 
-def Run_Diffusion(data, n_components=50, knn=30, n_jobs=-1, alpha=1, force_sparse = True):
-	"""Run Diffusion maps using the adaptive anisotropic kernel proposed by Setty et al, Nature Biotechnology 2019 - Characterization of cell fate probabilities in single-cell data with Palantir
-	:param data: Data matrix to diffuse from
-	:param n_components: Number of diffusion components to diffuse with
-	:param n_jobs: Number of threads to use in calculations
-	:param knn: Number of k-nearest-neighbors to use. The adaptive kernel will normalize distances by the k/2 neighbor distances, which is the median neighbor.
-	:param force_sparse: Whether to convert input data to the sparse format for speeding calculations.
-	:return: Multiscaled results, diffusion components, associated eigenvalues and suggested number of resulting components to use.
+def diffuse(data, n_components=50, knn=30, n_jobs=-1, alpha=1, force_sparse = True):
+	"""Runs Diffusion maps using an adaptation of the adaptive anisotropic kernel proposed by Setty et al, Nature Biotechnology 2019.
+	:param data: Data matrix to diffuse from. Either a sparse .coo or a dense pandas dataframe.
+	:param n_components: Number of diffusion components to compute. Defaults to 50. We suggest larger values if analyzing more than 10,000 cells.
+	:param n_jobs: Number of threads to use in calculations. Defaults to all but one.
+	:param knn: Number of k-nearest-neighbors to compute. The adaptive kernel will normalize distances by each cell distance of its median neighbor.
+	:param force_sparse: Whether to convert input data (coo or dataframe) to a csr sparse format for speeding calculations. Defaults to True. 
+	:return: Diffusion components, associated eigenvalues and suggested number of resulting components to use during Multiscaling.
 	"""
 	print('Converting input to sparse. Determing nearest neighbor graph...')
 	if force_sparse == True:
 		data = data.tocsr()
 		print('Converting input to sparse for efficiency. Determing nearest neighbor graph...')
-	
-		else:
-			print('Dense input. Determing nearest neighbor graph...')
+	else:
+		print('Dense input. Determing nearest neighbor graph...')
 
 	N = data.shape[0]
+	#Construct a k-nearest-neighbors graph
 	nbrs = NearestNeighbors(n_neighbors=int(knn), metric='euclidean', n_jobs=n_jobs).fit(data)
 	kNN = nbrs.kneighbors_graph(data, mode='distance')
-	# Adaptive k
+	# Adaptive k: distance to cell median nearest neighbors, used for kernel normalizaiton
 	adaptive_k = int(np.floor(knn / 2))
 	nbrs = NearestNeighbors(n_neighbors=int(adaptive_k), metric='euclidean', n_jobs=n_jobs).fit(data)
 	adaptive_std = nbrs.kneighbors_graph(data, mode='distance').max(axis=1)
 	adaptive_std = np.ravel(adaptive_std.todense())
 
-	# Kernel
-	x, y, dists = find(kNN)  
+	# Distance metrics
+	x, y, dists = find(kNN)  #k-nearest-neighbor distances
+	
 	# X, y specific stds
-	dists = dists / adaptive_std[x]
-	W = csr_matrix((np.exp(-dists), (x, y)), shape=[N, N])
+	dists = dists / adaptive_std[x] #Normalize by the distance of median nearest neighbor
+	W = csr_matrix((np.exp(-dists), (x, y)), shape=[N, N]) #Normalized distances
 
-
-	# Diffusion components
-	kernel = W + W.T
-	# Markov
+	#Kernel construction
+	kernel = W + W.T 
+	#Diffusion through Markov chain
 	D = np.ravel(kernel.sum(axis=1))
 	if alpha > 0:
 		# L_alpha
@@ -53,8 +52,11 @@ def Run_Diffusion(data, n_components=50, knn=30, n_jobs=-1, alpha=1, force_spars
 		D = np.ravel(kernel.sum(axis=1))
 
 	D[D != 0] = 1 / D[D != 0]
+	
+	#Setting the diffusion operator
 	T = csr_matrix((D, (range(N), range(N))), shape=[N, N]).dot(kernel)
-	# Eigen value dcomposition
+	
+	# Eigen value decomposition
 	D, V = eigs(T, n_components, tol=1e-4, maxiter=1000)
 	D = np.real(D)
 	V = np.real(V)
@@ -62,7 +64,7 @@ def Run_Diffusion(data, n_components=50, knn=30, n_jobs=-1, alpha=1, force_spars
 	D = D[inds]
 	V = V[:, inds]
 
-	# Normalize
+	# Normalize by the first diffusion component
 	for i in range(V.shape[1]):
 		V[:, i] = V[:, i] / np.linalg.norm(V[:, i])
 
@@ -83,23 +85,3 @@ def Run_Diffusion(data, n_components=50, knn=30, n_jobs=-1, alpha=1, force_spars
 	
 	return res
 	
-	
-def Multiscale(diff, n_eigs=None):
-	"""Determine multi scale space of the data
-	:param dm_res: Diffusion map results from run_diffusion_maps
-	:param n_eigs: Number of eigen vectors to use. If None specified, the number of eigen vectors will be determined using eigen gap
-	:return: Multi scale data matrix
-	"""
-	if n_eigs is None:
-		vals = np.ravel(diff['EigenValues'])
-		n_eigs = np.argsort(vals[:(len(vals) - 1)] - vals[1:])[-1] + 1
-		if n_eigs < 3:
-			n_eigs = np.argsort(vals[:(len(vals) - 1)] - vals[1:])[-2] + 1
-			
-	# Scale the data
-	use_eigs = list(range(1, n_eigs))
-	eig_vals = np.ravel(diff['EigenValues'][use_eigs])
-	data = diff['EigenVectors'].values[:,use_eigs] * (eig_vals / (1 - eig_vals))
-	data = pd.DataFrame(data, index=diff['EigenVectors'].index)
-	return data
-

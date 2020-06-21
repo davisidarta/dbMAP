c01d081552eb984b6fac7a8b026d2c4a122db367
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-06-21 03:32:28 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -91,7 +91,7 @@ class NMSlibTransformer(TransformerMixin, BaseEstimator):
 
         assert_array_almost_equal(Xt0.toarray(), Xt1.toarray(), decimal=5)
 
-def diffuse(data, n_components=100, knn=30, knn_dist='euclidean', ann=True, n_jobs=-1, alpha=1, force_sparse=True):
+def diffuse(data, n_components=100, knn=30, knn_dist='euclidean', ann=True, n_jobs=-1, alpha=1, sparse=True):
     """Runs Diffusion maps using an adaptation of the adaptive anisotropic kernel proposed by Setty et al,
         Nature Biotechnology 2019.
     :param data: Data matrix to diffuse from. Either a sparse .coo or a dense pandas dataframe.
@@ -106,8 +106,7 @@ def diffuse(data, n_components=100, knn=30, knn_dist='euclidean', ann=True, n_jo
     :param alpha: Alpha in the diffusion maps literature. Controls how much the results are biased by data distribution.
            Defaults to 1, which is suitable for normalized data.
     :param n_jobs: Number of threads to use in calculations. Defaults to all but one.
-    :param force_sparse: Whether to convert input data (coo or dataframe) to a csr sparse format for speeding
-           calculations. Defaults to True.
+    :param sparse: Whether input data is in a sparse format (i.e., csr or coo). Defaults to True.
     :return: Diffusion components, associated eigenvalues and suggested number of resulting components to use
              during Multiscaling.
     """

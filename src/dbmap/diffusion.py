c037a9565d3880b637444b0dd2ba76181a24bc23
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-06-21 01:25:49 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -4,7 +4,93 @@ from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix, find, issparse
 from scipy.sparse.linalg import eigs
 import hnswlib
-from . import ann
+
+import time
+import sys
+
+try:
+    import nmslib
+except ImportError:
+    print("The package 'nmslib' is required to run this example.")
+    sys.exit()
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.neighbors import KNeighborsTransformer
+from sklearn.utils._testing import assert_array_almost_equal
+
+print(__doc__)
+
+
+class NMSlibTransformer(TransformerMixin, BaseEstimator):
+    """Wrapper for using nmslib as sklearn's KNeighborsTransformer"""
+
+    def __init__(self, n_neighbors=5, metric='euclidean', method='sw-graph',
+                 n_jobs=1):
+        self.n_neighbors = n_neighbors
+        self.method = method
+        self.metric = metric
+        self.n_jobs = n_jobs
+
+    def fit(self, X):
+        self.n_samples_fit_ = X.shape[0]
+
+        # see more metric in the manual
+        # https://github.com/nmslib/nmslib/tree/master/manual
+        space = {
+            'sqeuclidean': 'l2',
+            'euclidean': 'l2',
+            'cosine': 'cosinesimil',
+            'l1': 'l1',
+            'l2': 'l2',
+            'hamming' : 'bit_hamming',
+            'jaccard' : 'bit_jaccard',
+            'jaccard_sparse' : 'jaccard_sparse',
+        }[self.metric]
+
+        self.nmslib_ = nmslib.init(method=self.method, space=space)
+        self.nmslib_.addDataPointBatch(X)
+        self.nmslib_.createIndex()
+        return self
+
+    def transform(self, X):
+        n_samples_transform = X.shape[0]
+
+        # For compatibility reasons, as each sample is considered as its own
+        # neighbor, one extra neighbor will be computed.
+        n_neighbors = self.n_neighbors + 1
+
+        results = self.nmslib_.knnQueryBatch(X, k=n_neighbors,
+                                             num_threads=self.n_jobs)
+        indices, distances = zip(*results)
+        indices, distances = np.vstack(indices), np.vstack(distances)
+
+        if self.metric == 'sqeuclidean':
+            distances **= 2
+
+        indptr = np.arange(0, n_samples_transform * n_neighbors + 1,
+                           n_neighbors)
+        kneighbors_graph = csr_matrix((distances.ravel(), indices.ravel(),
+                                       indptr), shape=(n_samples_transform,
+                                                       self.n_samples_fit_))
+
+        return kneighbors_graph
+
+    def test_transformers(self, X):
+        """Test that NMSlibTransformer and KNeighborsTransformer give same results
+        """
+        X = np.random.RandomState(42).randn(10, 2)
+
+        knn = KNeighborsTransformer()
+        Xt0 = knn.fit_transform(X)
+
+        nms = NMSlibTransformer()
+        Xt1 = nms.fit_transform(X)
+
+        assert_array_almost_equal(Xt0.toarray(), Xt1.toarray(), decimal=5)
+© 2020 GitHub, Inc.
 
 def diffuse(data, n_components=100, knn=30, knn_dist='euclidean', ann=True, n_jobs=-1, alpha=1, force_sparse=True):
     """Runs Diffusion maps using an adaptation of the adaptive anisotropic kernel proposed by Setty et al,

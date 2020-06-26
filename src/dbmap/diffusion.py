992af98c5d84e92f7ad4e12527958c8ba7d4f3ab
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-06-26 03:09:17 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -1,24 +1,18 @@
-import numpy as np
-import pandas as pd
-from sklearn.neighbors import NearestNeighbors
-from scipy.sparse import csr_matrix, find, issparse
-from scipy.sparse.linalg import eigs
-import numexpr as ne
 import time
 import sys
-from scipy.sparse import csr_matrix
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix, find, issparse
+from scipy.sparse.linalg import eigs, eigsh
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
 
 try:
     import nmslib
 except ImportError:
-    print("The package 'nmslib' is required to run accelerated dbMAP")
+    print("The package 'nmslib' is required to run accelerated dbMAP. Please install it 'with pip3 install nmslib'.")
     sys.exit()
-
-import numpy as np
-from scipy.sparse import csr_matrix
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.neighbors import NearestNeighbors
-from sklearn.model_selection import train_test_split
 
 print(__doc__)
 
@@ -28,12 +22,14 @@ class NMSlibTransformer(TransformerMixin, BaseEstimator):
 
     def __init__(self,
                  n_neighbors=30,
-                 metric='cosinesimil_sparse_fast',
+                 metric='angular_sparse',
                  method='hnsw',
                  n_jobs=-1,
                  M=30,
                  efC=100,
-                 efS=100):
+                 efS=100,
+                 p=None):
+
         self.n_neighbors = n_neighbors
         self.method = method
         self.metric = metric
@@ -41,16 +37,27 @@ class NMSlibTransformer(TransformerMixin, BaseEstimator):
         self.M = M
         self.efC = efC
         self.efS = efS
+        self.p = p
+        self.space = str = {
+            'euclidean': 'l2',
+            'euclidean_sparse': 'l2_sparse',
+            'cosine': 'cosinesimil',
+            'cosine_sparse': 'cosinesimil_sparse_fast',
+            'l1': 'l1',
+            'l1_sparse': 'l1_sparse',
+            'linf': 'linf',
+            'linf_sparse': 'linf_sparse',
+            'angular': 'angulardist',
+            'angular_sparse': 'angulardist_sparse_fast',
+            'levenshtein': 'leven',
+            'hamming': 'bit_hamming',
+            'jaccard': 'bit_jaccard',
+            'jaccard_sparse': 'jaccard_sparse',
+            'jansen-shan': 'jsmetrfastapprox'
+        }[self.metric]
+
 
     def fit(self, data):
-        self.n_samples_fit_ = data.shape[0]
-        self.space = str = {
-            'l2': 'l2',
-            'cosinesimil': 'cosinesimil',
-            'cosinesimil_sparse': 'cosinesimil_sparse',
-            'cosinesimil_sparse_fast': 'cosinesimil_sparse_fast',
-            'l1': 'l1',
-        }[self.metric]
         # see more metrics in the manual
         # https://github.com/nmslib/nmslib/tree/master/manual
 
@@ -69,18 +76,20 @@ class NMSlibTransformer(TransformerMixin, BaseEstimator):
             except SyntaxError:
                 print("Conversion to csr failed. Please provide a numpy array or a pandas dataframe.")
 
+        self.n_samples_fit_ = data.shape[0]
+
         index_time_params = {'M': self.M, 'indexThreadQty': self.n_jobs, 'efConstruction': self.efC, 'post': 0}
 
         self.nmslib_ = nmslib.init(method='hnsw',
-                                       space='cosinesimil_sparse_fast',
-                                       data_type=nmslib.DataType.SPARSE_VECTOR)
+                                   space='cosinesimil_sparse_fast',
+                                   data_type=nmslib.DataType.SPARSE_VECTOR)
 
         self.nmslib_.addDataPointBatch(data)
         start = time.time()
         self.nmslib_.createIndex(index_time_params)
         end = time.time()
-        print('Index-time parameters', 'M:', self.M, 'n_threads:', self.n_jobs, 'efConstruction:', self.efC , 'post:0')
-        print('Indexing time = %f (sec)' % (end-start))
+        print('Index-time parameters', 'M:', self.M, 'n_threads:', self.n_jobs, 'efConstruction:', self.efC, 'post:0')
+        print('Indexing time = %f (sec)' % (end - start))
         return self
 
     def transform(self, data):
@@ -156,39 +165,27 @@ class NMSlibTransformer(TransformerMixin, BaseEstimator):
         recall = recall / query_qty
         print('kNN recall %f' % recall)
 
+
 class Diffusor(TransformerMixin, BaseEstimator):
     """Sklearn estimator for using fast anisotropic diffusion with an anisotropic
     adaptive algorithm as proposed by Setty et al, 2018, and optimized by Sidarta-Oliveira, 2020.
-
-    :param data: Data matrix to diffuse from. Either a sparse .coo or a dense pandas dataframe.
-
     :param n_components: Number of diffusion components to compute. Defaults to 50. We suggest larger values if
            analyzing more than 10,000 cells.
-
-    :param knn: Number of k-nearest-neighbors to compute. The adaptive kernel will normalize distances by each cell
+    :param n_neighbors: Number of k-nearest-neighbors to compute. The adaptive kernel will normalize distances by each cell
            distance of its median neighbor.
-
     :param knn_dist: Distance metric for building kNN graph. Defaults to 'euclidean'. Users are encouraged to explore
-           different metrics, such as 'cosine' and 'jaccard'. The 'hamming' distance is also available for string
+           different metrics, such as 'cosine' and 'jaccard'. The 'hamming' and 'jaccard' distances are also available for string
            vectors.
-
     :param ann: Boolean. Whether to use approximate nearest neighbors for graph construction. Defaults to True.
-
     :param alpha: Alpha in the diffusion maps literature. Controls how much the results are biased by data distribution.
            Defaults to 1, which is suitable for normalized data.
-
     :param n_jobs: Number of threads to use in calculations. Defaults to all but one.
-
     :return: Diffusion components ['EigenVectors'], associated eigenvalues ['EigenValues'] and suggested number of resulting components to use
              during Multiscaling.
-
-
     Examples
     -------------
     >>>import dbmap
     # Fazer o resto do exemplo
-
-
     """
 
     def __init__(self,
@@ -197,10 +194,11 @@ class Diffusor(TransformerMixin, BaseEstimator):
                  alpha=1,
                  n_jobs=-2,
                  ann=True,
-                 ann_dist='cosinesimil_sparse_fast',
+                 ann_dist='angular_sparse',
                  M=30,
                  efC=100,
                  efS=100,
+                 knn_dist='euclidean',
                  ):
         self.n_components = n_components
         self.n_neighbors = n_neighbors
@@ -211,18 +209,26 @@ class Diffusor(TransformerMixin, BaseEstimator):
         self.M = M
         self.efC = efC
         self.efS = efS
+        self.knn_dist = knn_dist
+
     def fit_transform(self, data):
+        """Effectively computes on data.
+        :param data: input data. Takes in numpy arrays and scipy csr sparse matrices.
+        Please use with sparse data for top performance. You can adjust a series of
+        parameters that can make the process faster and more informational depending
+        on your dataset. Read more at https://github.com/davisidarta/dbmap
+        """
         start = time.time()
         N = data.shape[0]
         if self.ann == True:
             # Construct an approximate k-nearest-neighbors graph
-            anbrs = NMSlibTransformer(n_neighbors= self.n_neighbors,
-                 metric= self.ann_dist,
-                 method='hnsw',
-                 n_jobs=self.n_jobs,
-                 M=self.M,
-                 efC=self.efC,
-                 efS=self.efS)
+            anbrs = NMSlibTransformer(n_neighbors=self.n_neighbors,
+                                      metric=self.ann_dist,
+                                      method='hnsw',
+                                      n_jobs=self.n_jobs,
+                                      M=self.M,
+                                      efC=self.efC,
+                                      efS=self.efS)
             anbrs = anbrs.fit(data)
             akNN = anbrs.transform(data)
             # Adaptive k
@@ -238,16 +244,18 @@ class Diffusor(TransformerMixin, BaseEstimator):
             dists = dists / adaptive_std[x]  # Normalize by the distance of median nearest neighbor
             W = csr_matrix((np.exp(-dists), (x, y)), shape=[N, N])  # Normalized distances
         else:
+
             # Construct a k-nearest-neighbors graph
-            nbrs = NearestNeighbors(n_neighbors=int(self.n_neighbors), metric=knn_dist, n_jobs=n_jobs).fit(data)
-            kNN = nbrs.kneighbors_graph(data, mode='distance')
+            nbrs = NearestNeighbors(n_neighbors=int(self.n_neighbors), metric=self.knn_dist, n_jobs=self.n_jobs).fit(
+                data)
+            knn = nbrs.kneighbors_graph(data, mode='distance')
             # Adaptive k: distance to cell median nearest neighbors, used for kernel normalization.
             adaptive_k = int(np.floor(self.n_neighbors / 2))
-            nbrs = NearestNeighbors(n_neighbors=int(adaptive_k), metric='euclidean', n_jobs=n_jobs).fit(data)
+            nbrs = NearestNeighbors(n_neighbors=int(adaptive_k), metric='euclidean', n_jobs=self.n_jobs).fit(data)
             adaptive_std = nbrs.kneighbors_graph(data, mode='distance').max(axis=1)
             adaptive_std = np.ravel(adaptive_std.todense())
             # Distance metrics
-            x, y, dists = find(kNN)  # k-nearest-neighbor distances
+            x, y, dists = find(knn)  # k-nearest-neighbor distances
             # X, y specific stds
             dists = dists / adaptive_std[x]  # Normalize by the distance of median nearest neighbor
             W = csr_matrix((np.exp(-dists), (x, y)), shape=[N, N])  # Normalized distances
@@ -285,14 +293,20 @@ class Diffusor(TransformerMixin, BaseEstimator):
             res['EigenValues'] = pd.Series(res['EigenValues'])
             res['kernel'] = kernel
 
-        # Suggest a number of components to use
+        # Define a number of components to use
         vals = np.ravel(res['EigenValues'])
         n_eigs = np.argsort(vals[:(len(vals) - 1)] - vals[1:])[-1] + 1
-        if n_eigs < 3:
-            n_eigs = np.argsort(vals[:(len(vals) - 1)] - vals[1:])[-2] + 1
-        res['Suggested_eigs'] = n_eigs
+
+        # Scale the components
+        use_eigs = list(range(1, n_eigs))
+        eig_vals = np.ravel(res['EigenValues'][use_eigs])
+        data = res['EigenVectors'].values[:, use_eigs] * (eig_vals / (1 - eig_vals))
+        data = pd.DataFrame(data, index=res['EigenVectors'].index)
+        res['StructureComponents'] = data
+
         end = time.time()
         print('Total computation time=%f (sec), per sample=%f (sec), per sample adjusted for thread number=%f (sec)' %
-              (end-start, float(end-start)/N, self.n_jobs * float(end-start)/N))
+              (end - start, float(end - start) / N, self.n_jobs * float(end - start) / N))
 
         return res
+

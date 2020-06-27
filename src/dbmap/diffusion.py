dedec24464c1166a20f5f4326f99d86d97aa2181
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-06-27 02:45:59 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -4,255 +4,14 @@
 # contact: davisidarta@gmail.com
 ######################################
 import time
-import sys
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix, find, issparse
 from scipy.sparse.linalg import eigs
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.neighbors import NearestNeighbors
-from sklearn.model_selection import train_test_split
-from kneed import KneeLocator
-
-try:
-    import nmslib
-except ImportError:
-    print("The package 'nmslib' is required to run accelerated dbMAP. Please install it 'with pip3 install nmslib'.")
-    sys.exit()
 
 print(__doc__)
-
-
-class NMSlibTransformer(TransformerMixin, BaseEstimator):
-    """
-    Wrapper for using nmslib as sklearn's KNeighborsTransformer. This implements
-    an escalable approximate k-nearest-neighbors graph on spaces defined by nmslib.
-    Read more about nmslib and its various available metrics at
-    https://github.com/nmslib/nmslib.
-
-
-
-
-    """
-
-    def __init__(self,
-                 n_neighbors=30,
-                 metric='angular_sparse',
-                 method='hnsw',
-                 n_jobs=-1,
-                 M=30,
-                 efC=100,
-                 efS=100,
-                 p=None):
-        """
-        Initialize neighbour search parameters.
-        :param n_neighbors: number of nearest-neighbors to look for. In practice,
-        this should be considered the average neighborhood size and can vary depending
-        on your number of samples and data intrinsic dimensionality. Reasonable values
-        range from 5 to 100. Smaller values tend to lead to increased graph structure
-        resolution, but users should beware that a too low value may render granulated and vaguely
-        defined neighborhoods that arise as an artifact of downsampling. Larger values
-        will al
-
-         Defaults to 30.
-
-        """
-
-        self.n_neighbors = n_neighbors
-        self.method = method
-        self.metric = metric
-        self.n_jobs = n_jobs
-        self.M = M
-        self.efC = efC
-        self.efS = efS
-        self.p = p
-        self.space = str = {
-            'sqeuclidean': 'l2',
-            'euclidean': 'l2',
-            'euclidean_sparse': 'l2_sparse',
-            'cosine': 'cosinesimil',
-            'cosine_sparse': 'cosinesimil_sparse_fast',
-            'l1': 'l1',
-            'l1_sparse': 'l1_sparse',
-            'linf': 'linf',
-            'linf_sparse': 'linf_sparse',
-            'angular': 'angulardist',
-            'angular_sparse': 'angulardist_sparse_fast',
-            'levenshtein': 'leven',
-            'hamming': 'bit_hamming',
-            'jaccard': 'bit_jaccard',
-            'jaccard_sparse': 'jaccard_sparse',
-            'jansen-shan': 'jsmetrfastapprox'
-        }[self.metric]
-
-    def fit(self, data):
-        # see more metrics in the manual
-        # https://github.com/nmslib/nmslib/tree/master/manual
-
-        if issparse(data) == True:
-            print('Sparse input. Proceding without converting...')
-        if issparse(data) == False:
-            print('Converting input to sparse...')
-            try:
-                data = data.tocsr()
-            except SyntaxError:
-                print("Conversion to csr failed. Please provide a numpy array or a pandas dataframe."
-                      "Trying internal construction...")
-                sys.exit()
-            try:
-                data = csr_matrix(data)
-            except SyntaxError:
-                print("Conversion to csr failed. Please provide a numpy array or a pandas dataframe.")
-
-        self.n_samples_fit_ = data.shape[0]
-
-        index_time_params = {'M': self.M, 'indexThreadQty': self.n_jobs, 'efConstruction': self.efC, 'post': 0}
-
-        self.nmslib_ = nmslib.init(method='hnsw',
-                                   space='cosinesimil_sparse_fast',
-                                   data_type=nmslib.DataType.SPARSE_VECTOR)
-
-        self.nmslib_.addDataPointBatch(data)
-        start = time.time()
-        self.nmslib_.createIndex(index_time_params)
-        end = time.time()
-        print('Index-time parameters', 'M:', self.M, 'n_threads:', self.n_jobs, 'efConstruction:', self.efC, 'post:0')
-        print('Indexing time = %f (sec)' % (end - start))
-        return self
-
-    def transform(self, data):
-        start = time.time()
-        n_samples_transform = data.shape[0]
-        query_time_params = {'efSearch': self.efS}
-        print('Setting query-time parameters:', query_time_params)
-        self.nmslib_.setQueryTimeParams(query_time_params)
-
-        # For compatibility reasons, as each sample is considered as its own
-        # neighbor, one extra neighbor will be computed.
-        self.n_neighbors = self.n_neighbors + 1
-
-        results = self.nmslib_.knnQueryBatch(data, k=self.n_neighbors,
-                                             num_threads=self.n_jobs)
-
-        indices, distances = zip(*results)
-        indices, distances = np.vstack(indices), np.vstack(distances)
-
-        query_qty = data.shape[0]
-
-        if self.metric == 'sqeuclidean':
-            distances **= 2
-
-        indptr = np.arange(0, n_samples_transform * self.n_neighbors + 1,
-                           self.n_neighbors)
-        kneighbors_graph = csr_matrix((distances.ravel(), indices.ravel(),
-                                       indptr), shape=(n_samples_transform,
-                                                       self.n_samples_fit_))
-        end = time.time()
-        print('kNN time total=%f (sec), per query=%f (sec), per query adjusted for thread number=%f (sec)' %
-              (end - start, float(end - start) / query_qty, self.n_jobs * float(end - start) / query_qty))
-
-        return kneighbors_graph
-
-    def test_efficiency(self, data, data_use=0.1):
-        """Test that NMSlibTransformer and KNeighborsTransformer give same results
-        """
-        self.data_use = data_use
-
-        query_qty = data.shape[0]
-
-        (dismiss, test) = train_test_split(data, test_size=self.data_use)
-        query_time_params = {'efSearch': self.efS}
-        print('Setting query-time parameters', query_time_params)
-        self.nmslib_.setQueryTimeParams(query_time_params)
-
-        # For compatibility reasons, as each sample is considered as its own
-        # neighbor, one extra neighbor will be computed.
-        self.n_neighbors = self.n_neighbors + 1
-        start = time.time()
-        ann_results = self.nmslib_.knnQueryBatch(data, k=self.n_neighbors,
-                                                 num_threads=self.n_jobs)
-        end = time.time()
-        print('kNN time total=%f (sec), per query=%f (sec), per query adjusted for thread number=%f (sec)' %
-              (end - start, float(end - start) / query_qty, self.n_jobs * float(end - start) / query_qty))
-
-        # Use sklearn for exact neighbor search
-        start = time.time()
-        nbrs = NearestNeighbors(n_neighbors=self.n_neighbors,
-                                metric='cosine',
-                                algorithm='brute').fit(data)
-        knn = nbrs.kneighbors(data)
-        end = time.time()
-        print('brute-force gold-standart kNN time total=%f (sec), per query=%f (sec)' %
-              (end - start, float(end - start) / query_qty))
-
-        recall = 0.0
-        for i in range(0, query_qty):
-            correct_set = set(knn[1][i])
-            ret_set = set(ann_results[i][0])
-            recall = recall + float(len(correct_set.intersection(ret_set))) / len(correct_set)
-        recall = recall / query_qty
-        print('kNN recall %f' % recall)
-
-
-class multiscale(TransformerMixin, BaseEstimator):
-
-    def __init__(self,
-                 n_eigs=None,
-                 sensitivity=1,
-                 plot=False
-                 ):
-        """Determine multi scale space of the data
-        :param n_eigs: Number of eigen vectors to use. If None specified, the number
-               of eigen vectors will be determined using eigen gap identification.
-        :param sensitivity: sensitivity of eigen gap identification. Defaults to 1.
-        :param plot: Whether to plot or not the scree plot of information entropy.
-        :return: Multi scaled data matrix
-        """
-        self.n_eigs = n_eigs
-        self.sensitivity = sensitivity
-        self.plot = plot
-
-    def fit(self, res):
-        if self.n_eigs != None:
-            if self.plot == True:
-                ev = res['EigenValues']
-                x = range(1, len(ev)+1)
-                y = ev
-                kneedle = KneeLocator(x, y, S=self.sensitivity, curve='convex', direction='decreasing')
-                kneedle.plot_knee()
-
-        if self.n_eigs == None:
-            ev = res['EigenValues']
-            x = range(1, len(ev)+1)
-            y = ev
-
-            kneedle = KneeLocator(x, y, S=self.sensitivity, curve='convex', direction='decreasing')
-            self.n_eigs = round(kneedle.knee)
-
-            print('Automatically selected and multiscaled ' + str(round(kneedle.knee)) +
-                  ' diffusion components.')
-
-            if self.plot == True:
-                kneedle.plot_knee()
-
-        return self
-
-    def transform(self, res):
-
-        if self.n_eigs == None:
-            vals = np.ravel(res["EigenValues"])
-            self.n_eigs = np.argsort(vals[: (len(vals) - 1)] - vals[1:])[-1] + 1
-            if self.n_eigs < 3:
-                self.n_eigs = np.argsort(vals[: (len(vals) - 1)] - vals[1:])[-2] + 2
-
-        # Scale the data
-        use_eigs = list(range(1, self.n_eigs))
-        eig_vals = np.ravel(res["EigenValues"][use_eigs])
-        mms = res["EigenVectors"].values[:, use_eigs] * (eig_vals / (1 - eig_vals))
-        mms = pd.DataFrame(mms)
-
-        return mms
-
 
 class Diffusor(TransformerMixin, BaseEstimator):
     """Sklearn estimator for using fast anisotropic diffusion with an anisotropic
@@ -287,6 +46,7 @@ class Diffusor(TransformerMixin, BaseEstimator):
                  efC=100,
                  efS=100,
                  knn_dist='euclidean',
+                 sensitivity = 2
                  ):
         self.n_components = n_components
         self.n_neighbors = n_neighbors
@@ -298,6 +58,8 @@ class Diffusor(TransformerMixin, BaseEstimator):
         self.efC = efC
         self.efS = efS
         self.knn_dist = knn_dist
+        self.sensitivity = sensitivity
+
 
     def fit_transform(self, data,
                       plot_knee=False):
@@ -385,10 +147,10 @@ class Diffusor(TransformerMixin, BaseEstimator):
         res["EigenValues"] = pd.Series(res["EigenValues"])
         res['kernel'] = kernel
 
-        #multi = multiscale(n_eigs=None, plot=self.plot_knee)
-        #mms = multi.fit(res)
-        #mms = mms.transform(res)
-        #res['StructureComponents'] = mms
+        multi = multiscale(n_eigs=None, plot=self.plot_knee, sensitivity=self.sensitivity)
+        mms = multi.fit(res)
+        mms = mms.transform(res)
+        res['StructureComponents'] = mms
 
         end = time.time()
         print('Total computation time=%f (sec), per sample=%f (sec), per sample adjusted for thread number=%f (sec)' %

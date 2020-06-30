fa9933fbc862af797a70696a669943ae5df97e73
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-06-30 00:21:41 -0300

    Update utils.py

--- src/dbmap/utils.py
+++ src/dbmap/utils.py
@@ -1,19 +1,36 @@
 """
 Utility functions
 """
+from collections import namedtuple
+import logging
+import warnings
 
-import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy.sparse as sps
+import numba as n
+from numba import njit, jitclass, prange
+
+@n.njit(parallel=True)
+def fast_knn_indices(X, n_neighbors):
+    """A fast computation of knn indices.
+    Parameters
+    ----------
+    X: array of shape (n_samples, n_features)
+        The input data to compute the k-neighbor indices of.
+    n_neighbors: int
+        The number of nearest neighbors to compute for each sample in ``X``.
+    Returns
+    -------
+    knn_indices: array of shape (n_samples, n_neighbors)
+        The indices on the ``n_neighbors`` closest points in the dataset.
+    """
+    knn_indices = np.empty((X.shape[0], n_neighbors), dtype=np.int32)
+    for row in n.prange(X.shape[0]):
+        # v = np.argsort(X[row])  # Need to call argsort this way for numba
+        v = X[row].argsort(kind="quicksort")
+        v = v[:n_neighbors]
+        knn_indices[row] = v
+    return knn_indices
 
 
-def dbmap_plot(res, title=None, fontsize=18, labels=None, pt_size=None, marker='o', opacity=1):
-    plt.scatter(
-        res[:, 0],
-        res[:, 1],
-    s=pt_size,
-    c=labels,
-    marker=marker,
-    alpha=opacity
-    )
-    plt.gca().set_aspect('equal', 'datalim')
-    plt.title(title, fontsize=fontsize)
-                        

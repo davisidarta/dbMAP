77c769dc51b3df1054caed892bf479bda715a520
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-07-16 04:26:21 -0300

    Remove temporary knee-based  entropy

--- src/dbmap/multiscale.py
+++ src/dbmap/multiscale.py
@@ -1,34 +1,29 @@
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-from kneed import KneeLocator
 
 class multiscale(TransformerMixin, BaseEstimator):
 
     def __init__(self,
-                 n_eigs=None,
-                 sensitivity=1,
-                 plot=False
+                 n_eigs=None
                  ):
         """Determine multi scale space of the data
         :param n_eigs: Number of eigen vectors to use. If None specified, the number
                of eigen vectors will be determined using eigen gap identification.
-        :param sensitivity: sensitivity of eigen gap identification. Defaults to 1.
-        :param plot: Whether to plot or not the scree plot of information entropy.
         :return: Multi scaled data matrix
         """
         self.n_eigs = n_eigs
-        self.sensitivity = sensitivity
-        self.plot = plot
+       # self.sensitivity = sensitivity
+       # self.plot = plot
 
     def fit(self, res):
-        if self.n_eigs != None:
-            if self.plot == True:
-                ev = res['EigenValues']
-                x = range(1, len(ev)+1)
-                y = ev
-                kneedle = KneeLocator(x, y, S=self.sensitivity, curve='convex', direction='decreasing')
-                kneedle.plot_knee()
+        # if self.n_eigs != None:
+          #  if self.plot == True:
+           #     ev = res['EigenValues']            # TODO: implement an optional knee selector
+            #    x = range(1, len(ev)+1)
+             #   y = ev
+              #  kneedle = KneeLocator(x, y, S=self.sensitivity, curve='convex', direction='decreasing')
+               # kneedle.plot_knee()
 
         if self.n_eigs is None:
             vals = np.ravel(res["EigenValues"])

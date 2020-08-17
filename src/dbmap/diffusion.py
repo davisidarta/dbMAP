36a82263a4ed7ef2e44367e43b1d683950fc89e1
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-08-17 04:32:50 -0300

    Update diffusion.py

--- src/dbmap/diffusion.py
+++ src/dbmap/diffusion.py
@@ -2,7 +2,6 @@
 # Author: Davi Sidarta-Oliveira
 # School of Medical Sciences,University of Campinas,Brazil
 # contact: davisidarta@gmail.com
-# Please note that this code has several contributions from Manu Setty et al, Nature
 ######################################
 import time
 import numpy as np

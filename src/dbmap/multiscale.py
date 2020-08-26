a3775893e031e8c3c1bf2f3a2717c17c9ec00a3e
davisidarta <44589562+davisidarta@users.noreply.github.com>
2020-08-26 02:39:35 -0300

    Update multiscale.py

--- src/dbmap/multiscale.py
+++ src/dbmap/multiscale.py
@@ -1,39 +1,22 @@
 import numpy as np
 import pandas as pd
-from kneed import KneeLocator
 
-def multiscale(res, n_eigs=None, sensitivity=10, verbose=True):
+def multiscale(res, n_eigs=None):
     """Determine multi scale space of the data
           :param n_eigs: Number of eigen vectors to use. If None specified, the number
                  of eigen vectors will be determined using eigen gap identification.
           :return: Multi scaled data matrix
     """
     if n_eigs is None:
-        vals = np.ravel(res["EigenValues"])
-        n_eigs = np.argsort(vals[: (len(vals) - 1)] - vals[1:])[-1] + 1
-        if n_eigs < 3:
-            n_eigs = np.argsort(vals[: (len(vals) - 1)] - vals[1:])[-2] + 3
-
-        if n_eigs < 30:
-            if verbose:
-                print('Selecting number of components based on potential curve.')
-            # Try selecting n_eigs based on the second derivative
-            vals = np.array(res['EigenValues'])
-            k_use = range(len(vals))
-            kn = KneeLocator(list(k_use), vals, S=sensitivity, curve='convex', direction='decreasing')
-            n_eigs = kn.knee
-
-    # Multiscale diffusion - account for all t's
-    use_eigs = list(range(1, n_eigs))
-    eig_vals = np.ravel(res["EigenValues"][use_eigs])
-    data = res["EigenVectors"].values[:, use_eigs] * (eig_vals / (1 - eig_vals))
-
-    data = pd.DataFrame(data, index=res["EigenVectors"].index)
-    if verbose:
-        if n_eigs is not None:
-            print('Selected and multiscaled ' + str(round(n_eigs)) +
-                  ' diffusion components.')
-        else:
-            print('Automatically selected and multiscaled ' + str(round(n_eigs)) +
-                  ' diffusion components.')
-    return data
+        vals = np.array(res["EigenValues"])
+        n_eigs = np.sum( vals > 0, axis=0)
+    # Multiscale diffusion
+    use_eigs = list(range(0, n_eigs))
+    ev = res['EigenValues']
+    eig_vals = np.ravel(ev.reindex(use_eigs))
+    ms_data = res["EigenVectors"].values[:, use_eigs] * (eig_vals / (1 - eig_vals))
+    ms_data = pd.DataFrame(ms_data, index=res["EigenVectors"].index)
+    print('Automatically selected and multiscaled ' + str(round(n_eigs)) +
+          ' diffusion components.')
+    return ms_data
+          

24efe02743ec070570ec4d524dffa73e6df14ce8
davisidarta <davisidarta@gmail.com>
2021-02-25 14:49:16 -0300

    Quick fix

--- dbmap/utils.py
+++ dbmap/utils.py
@@ -1,35 +1,3 @@
-"""
-Utility functions
-"""
-
-import pandas as pd
-import numpy as np
-import scipy.sparse as sps
-import numba as n
-
-@n.njit(parallel=True)
-def fast_knn_indices(X, n_neighbors):
-    """A fast computation of knn indices.
-    Parameters
-    ----------
-    X: array of shape (n_samples, n_features)
-        The input data to compute the k-neighbor indices of.
-    n_neighbors: int
-        The number of nearest neighbors to compute for each sample in ``X``.
-    Returns
-    -------
-    knn_indices: array of shape (n_samples, n_neighbors)
-        The indices on the ``n_neighbors`` closest points in the dataset.
-    """
-    knn_indices = np.empty((X.shape[0], n_neighbors), dtype=np.int32)
-    for row in n.prange(X.shape[0]):
-        # v = np.argsort(X[row])  # Need to call argsort this way for numba
-        v = X[row].argsort(kind="quicksort")
-        v = v[:n_neighbors]
-        knn_indices[row] = v
-    return knn_indices
-
-
 # Author: Leland McInnes <leland.mcinnes@gmail.com>
 #
 # License: BSD 3 clause
